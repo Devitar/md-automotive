4697e7c1523e874b43752406cfb2eb4af713adaa
Devitar <devin.curtis1210@gmail.com>
2022-06-26 18:11:08 -0600

    Added basic routing, cleaned up unused files

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HomePage } from "./views";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      {/* TODO: Additional routes */}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>
+);
